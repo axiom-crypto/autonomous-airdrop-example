7b3032ab7bf2ef9da9446228ac505f976efe98ef
roshan <19766713+rpalakkal@users.noreply.github.com>
2024-02-09 14:44:58 -0500

    feat: axiom-std

--- test/AutonomousAirdrop.t.sol
+++ test/AutonomousAirdrop.t.sol
@@ -1,46 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-import { AxiomTest, AxiomVm } from "@axiom-crypto/v2-periphery/test/AxiomTest.sol";
+import "@axiom-crypto/axiom-std/AxiomTest.sol";
 import { AutonomousAirdrop } from "../src/AutonomousAirdrop.sol";
 import { UselessToken } from "../src/UselessToken.sol";
 
 contract AutonomousAirdropTest is AxiomTest {
+    using Axiom for Query;
+
+    struct AxiomInput {
+        uint64 blockNumber;
+        uint64 txIdx;
+        uint64 logIdx;
+    }
+
     address public constant SWAP_SENDER_ADDR = 0xf591C4c1e179A5E16407116882f7F8a524D51d14;
     AutonomousAirdrop autonomousAirdrop;
     UselessToken uselessToken;
+    AxiomInput public defaultInput;
+    bytes32 public querySchema;
 
     function setUp() public {
         _createSelectForkAndSetupAxiom("sepolia", 5_103_100);
+
+        defaultInput = AxiomInput({
+            blockNumber: 5_130_226,
+            txIdx: 40,
+            logIdx: 2
+        });
         
-        inputPath = "app/axiom/data/inputs.json";
-        querySchema = axiomVm.compile("app/axiom/swapEvent.circuit.ts", inputPath);
+        querySchema = axiomVm.readCircuit("app/axiom/swapEvent.circuit.ts", abi.encode(defaultInput));
 
         autonomousAirdrop = new AutonomousAirdrop(axiomV2QueryAddress, uint64(block.chainid), querySchema);
         uselessToken = new UselessToken(address(autonomousAirdrop));
         autonomousAirdrop.updateAirdropToken(address(uselessToken));
     }
 
-    function test_axiomSendQuery() public {
-        AxiomVm.AxiomSendQueryArgs memory args =
-            axiomVm.sendQueryArgs(inputPath, address(autonomousAirdrop), callbackExtraData, feeData);
+        /// @dev Simple demonstration of testing an Axiom client contract using Axiom cheatcodes
+    function test_simple_example() public {
+        // create a query into Axiom with default parameters
+        Query memory q = query(querySchema, abi.encode(defaultInput), address(autonomousAirdrop));
 
-        axiomV2Query.sendQuery{ value: args.value }(
-            args.sourceChainId,
-            args.dataQueryHash,
-            args.computeQuery,
-            args.callback,
-            args.feeData,
-            args.userSalt,
-            args.refundee,
-            args.dataQuery
-        );
+        // send the query to Axiom
+        q.send();
+
+        require(autonomousAirdrop.hasClaimed(SWAP_SENDER_ADDR) == false, "User has already claimed this airdrop");
+        uint256 prevBalance = uselessToken.balanceOf(SWAP_SENDER_ADDR);
+
+        // prank fulfillment of the query, returning the Axiom results 
+        bytes32[] memory results = q.prankFulfill(SWAP_SENDER_ADDR);
+
+        // parse Axiom results and verify length is as expected
+        assertEq(results.length, 3);
+        address userEventAddress = address(uint160(uint256(results[0])));
+
+        // verify the user claims the airdrop
+        require(SWAP_SENDER_ADDR == userEventAddress, "Invalid user address for event");
+        require(autonomousAirdrop.hasClaimed(userEventAddress), "User did not claim this airdrop");
+        require(uselessToken.balanceOf(SWAP_SENDER_ADDR)  == prevBalance + 100 * 10 ** 18, "User did not receive 100 tokens");
     }
 
-    function test_axiomCallback() public {
-        AxiomVm.AxiomFulfillCallbackArgs memory args =
-            axiomVm.fulfillCallbackArgs(inputPath, address(autonomousAirdrop), callbackExtraData, feeData, SWAP_SENDER_ADDR);
-        
-        axiomVm.prankCallback(args);
-    }
 }

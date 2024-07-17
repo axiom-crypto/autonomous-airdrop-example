829326fbafcb0653ff117095deea35d1eb14d84a
Yu Jiang Tham <yoojbruin@gmail.com>
2024-07-16 23:46:44 -0400

    Update to client SDK v2.1.0

--- app/src/app/axiomProvider.tsx
+++ app/src/app/axiomProvider.tsx
@@ -15,7 +15,7 @@ export default function AxiomProvider({
   return (
     <AxiomCircuitProvider
       compiledCircuit={compiledCircuit}
-      provider={process.env.NEXT_PUBLIC_ALCHEMY_URI_SEPOLIA as string}
+      rpcUrl={process.env.NEXT_PUBLIC_ALCHEMY_URI_SEPOLIA as string}
       chainId={"11155111"}
     >
       {mounted && children}
